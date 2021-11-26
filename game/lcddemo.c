2ce400cf50c20c7916a9715a0a3ccad6b50377ae
Madison Anderson <mranderson@miners.utep.edu>
2021-11-26 14:45:41 -0700

    flashing white git push

--- game/lcddemo.c
+++ game/lcddemo.c
@@ -6,8 +6,46 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
+#define LED BIT6		/* note that bit zero req'd for display */
+
+#define SW1 1
+#define SW2 2
+#define SW3 4
+#define SW4 8
+
+#define SWITCHES 15
+
+static char 
+switch_update_interrupt_sense()
+{
+  char p2val = P2IN;
+  /* update switch interrupt to detect changes from current buttons */
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
+}
+
+void 
+switch_init()			/* setup switch */
+{  
+  P2REN |= SWITCHES;		/* enables resistors for switches */
+  P2IE |= SWITCHES;		/* enable interrupts from switches */
+  P2OUT |= SWITCHES;		/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
+  switch_update_interrupt_sense();
+}
+
+int switches = 0;
+
+void
+switch_interrupt_handler()
+{
+  char p2val = switch_update_interrupt_sense();
+  switches = ~p2val & SWITCHES;
+}
 
 short redrawScreen = 1;
+
 void wdt_c_handler()
 {
   static int secCount = 0;
@@ -19,19 +57,66 @@ void wdt_c_handler()
   }
 }
 
+void update_shape();
+
 /** Initializes everything, clears the screen, draws "hello" and a square */
 int
 main()
 {
   configureClocks();
   lcd_init();
+  switch_init();
   enableWDTInterrupts();
   u_char width = screenWidth, height = screenHeight;
 
-  clearScreen(COLOR_BLUE);
+  clearScreen(COLOR_WHITE);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  // drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  // fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  or_sr(0x8);
   
+  while (1) {
+    if(redrawScreen) {
+      redrawScreen = 0;
+      update_shape();
+    }
+    or_sr(0x10);
+  }
+ 
+}
+
+void
+update_shape()
+{
+  static unsigned char row = screenHeight / 2, col = screenWidth / 2;
+  static char blue = 31, green = 0, red = 31;
+  static unsigned char step = 0;
+  if (switches & SW4) return;
+  if (step <= 60) {
+    int startCol = col - step;
+    int endCol = col + step;
+    int width = 1 + endCol - startCol;
+    // a color in this BGR encoding is BBBB BGGG GGGR RRRR
+    unsigned int color = (blue << 11) | (green << 5) | red;
+    fillRectangle(startCol, row+step, width, 1, color);
+    fillRectangle(startCol, row-step, width, 1, color);
+    if (switches & SW3) green = (green + 1) % 64;
+    if (switches & SW2) blue = (blue + 2) % 32;
+    if (switches & SW1) red = (red - 3) % 32;
+    step ++;
+  } else {
+     clearScreen(COLOR_WHITE);
+     step = 0;
+  }
+}
+
+
+/* Switch on S2 */
+void
+__interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
 }
